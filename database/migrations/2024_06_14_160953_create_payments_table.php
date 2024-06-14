d325ca3d44a763a396533a548766ed2c96c2daaa
H. Ackerman <argentina@esblan.ca>
2024-06-14 13:28:28 -0300

    a

--- database/migrations/2024_06_14_160953_create_payments_table.php
+++ database/migrations/2024_06_14_160953_create_payments_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->float('product_price');
             $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('order_id');
         });
     }
 
