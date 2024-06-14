29c02524efc0328eccd8c9dac74efb65aec74804
H. Ackerman <argentina@esblan.ca>
2024-06-14 12:25:20 -0300

    Start

---
+++ database/migrations/2024_06_13_234239_create_people_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('cpf');
+            $table->timestamp('birth_date');
+            $table->timestamps();
+            $table->unsignedBigInteger('area_code');
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
